1b7ce130307765f53b6f2586561dc92ce17f5c31
johnmart19 <johnivan19999@gmail.com>
2021-12-15 21:53:06 +0800

    sm8250-common: common: Add missing android.hardware.media.omx
    Fixes: android.hardware.media.omx@1.0-service: Service not available yet
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -363,6 +363,7 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-service \
     init.qti.media.sh \
     libOmxAacEnc \
     libOmxAmrEnc \
@@ -372,7 +373,8 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_omx.vendor
 
 # Power
 PRODUCT_PACKAGES += \
