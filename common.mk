140ffc0cd981855818e3673acdbc311c6c2533bb
cjh1249131356 <cjh1249131356@gmail.com>
2022-08-16 18:30:30 +0800

    sm8250-common: Add support for OnePlus Camera & Gallery
    * Camera repo: https://gitlab.com/cjh1249131356/vendor_oneplus_camera
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- common.mk
+++ common.mk
@@ -108,6 +108,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera_metadata_shim \
+    libcamera2ndk_vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_COPY_FILES += \
@@ -320,6 +321,13 @@ PRODUCT_PACKAGES += \
     OPlusSystemUIResCommon \
     WifiResTarget
 
+# Oplus-fwk
+PRODUCT_PACKAGES += \
+    oplus-fwk
+
+PRODUCT_BOOT_JARS += \
+    oplus-fwk
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
@@ -470,6 +478,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+# Inherit from the OnePlus Camera makefile.
+$(call inherit-product, vendor/oneplus/camera/camera-vendor.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
