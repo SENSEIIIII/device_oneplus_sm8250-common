a22b299f6362514649abd8b46cb07713762e86d9
SENSEIIIII <mistakesensei@gmail.com>
2023-10-09 19:06:29 +0000

    patch odm/lib64/libdmtpclient.so

--- extract-files.sh
+++ extract-files.sh
@@ -94,6 +94,9 @@ function blob_fixup() {
         vendor/bin/sensors.qti)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
             ;;
+        odm/lib64/libdmtpclient.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
     esac
 }
 
