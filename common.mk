cf088c22b02cba2329f17e3f0fba92ac1b94e1b9
SENSEIIIII <mistakesensei@gmail.com>
2023-10-10 03:55:18 +0000

    patch vendor/lib64/libsnsdiaglog.so

--- common.mk
+++ common.mk
@@ -358,7 +358,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge \
     sensors.oplus
 
