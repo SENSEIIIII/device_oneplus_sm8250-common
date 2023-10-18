09c7e88c62121ad02831e9db48dc5fffb0c1ffe3
SENSEIIIII <mistakesensei@gmail.com>
2023-10-18 15:47:26 +0000

    Revert "sm8250-common: Switch to oplus sensors multihal"
    This reverts commit 5bdd02ce67cbb92291376ad9f1cabd42bd2378cf.

--- common.mk
+++ common.mk
@@ -354,7 +354,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal.oplus \
+    android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge \
     sensors.oplus
 
