ac54ef1575bfa67833dcf47df5da022fc49d00cc
Pranav Vashi <neobuddy89@gmail.com>
2025-09-11 13:18:39 +0530

    sm8250-common: DeviceSettings: Switch to material expressive design layout
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- DeviceSettings/src/org/lineageos/device/DeviceSettings/DeviceSettingsActivity.java
+++ DeviceSettings/src/org/lineageos/device/DeviceSettings/DeviceSettingsActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2022 crDroid Android Project
+ * Copyright (C) 2018-2024 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,13 +33,12 @@ import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 
 public class DeviceSettingsActivity extends CollapsingToolbarBaseActivity {
 
-    private static final String TAG = "DeviceSettings";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
-                new DeviceSettings(), TAG).commit();
+        getSupportFragmentManager().beginTransaction().replace(
+                com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+                new DeviceSettings()).commit();
     }
 }
