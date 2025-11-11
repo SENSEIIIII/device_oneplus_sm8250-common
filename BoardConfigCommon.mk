069fd4ab1b450ddc728b7cbd2cc1a5c8a69077b4
SENSEIIIII <mistakesensei@gmail.com>
2025-11-11 15:28:58 +0000

    sm8250-common: temp add rule BUILD_BROKEN_DUP_RULES := true

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -5,6 +5,7 @@
 #
 
 COMMON_PATH := device/oneplus/sm8250-common
+BUILD_BROKEN_DUP_RULES := true
 
 # A/B
 AB_OTA_UPDATER := true
@@ -91,7 +92,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := BRAND_SHOW_FLAG=oneplus
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
-TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig vendor/oplus.config
+TARGET_KERNEL_CONFIG := vendor/meteoric_defconfig
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
