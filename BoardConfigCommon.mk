0b70b44a71c0147d379d448cf8ec26d38aea01b5
SENSEIIIII <mistakesensei@gmail.com>
2025-11-12 04:02:23 +0000

    sm8250-common: Introduce remove packages and vendor props for dolby support

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -70,7 +70,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/oplus/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 2
